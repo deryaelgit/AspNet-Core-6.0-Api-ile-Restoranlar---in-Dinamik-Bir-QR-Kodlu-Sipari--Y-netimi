8d2dbeda6eb22af9d99acb25c897c74f332d1c77
Derya <deryaelgit@hotmail.com>
2024-09-18 13:50:22 +0300

    EntityEklemeleri

--- SignalRProject/DataAccessLayer/Concrete/AppContext.cs
+++ SignalRProject/DataAccessLayer/Concrete/AppContext.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Concrete
 {
-    public class AppContext
+    public class AppContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+
+        public DbSet<Testimonial> Testimonials { get; set; }
+
     }
 }
--- SignalRProject/EntityLayer/Entities/Discount.cs
+++ SignalRProject/EntityLayer/Entities/Discount.cs
@@ -1,6 +1,11 @@
-﻿namespace SignalRApi.DAL.Entities
+﻿namespace EntityLayer.Entities
 {
     public class Discount
     {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount    { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
